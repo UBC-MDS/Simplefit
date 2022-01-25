6d09160070af8b8f7c5dd6d0bc284182abe0b9a3
Reza Mirzazadeh <mrezamirzazadeh@gmail.com>
2022-01-24 18:09:18 -0800

    Changed the regressor test

--- tests/test_regressor.py
+++ tests/test_regressor.py
@@ -13,9 +13,13 @@ def test_regressor():
     #                         numeric_feats=['acousticness', 'danceability', 'duration_ms'],
     #                         categorical_feats=['genre'])
     # actual_test_score = regressor_df.loc["test_score"].tolist()
-    # expected_test_score = [-0.0015605122150153417, 0.7312349806310714, 0.7313441392629443, 0.731393854470928]
+    # actual_test_score = [round(num, 2) for num in actual_test_score]
+    # expected_test_score = [-0.00, 0.73, 0.73, 0.73]
+    
     # actual_train_score = regressor_df.loc["train_score"].tolist()
-    # expected_train_score = [0.0, 0.7347815872733937, 0.7349114231911118, 0.734915756374242]
+    # actual_train_score = [round(num, 2) for num in actual_train_score]
+    # expected_train_score = [0.0, 0.73, 0.73, 0.73]
+
     # assert actual_test_score == expected_test_score, "regressor modeled incorrectly test scores are not equal to what they should be!"
     # assert actual_train_score == expected_train_score, "regressor modeled incorrectly train scores are not equal to what they should be!"
 
@@ -26,9 +30,12 @@ def test_regressor():
                             numeric_feats=['acousticness', 'danceability', 'duration_ms'],
                             categorical_feats=['genre'])
     actual_test_score = regressor_df.loc["test_score"].tolist()
-    expected_test_score = [-0.0015605122150153417, 0.7312349350768665, 0.7313442775914065, 0.731393854472432]
+    actual_test_score = [round(num, 2) for num in actual_test_score]
+    expected_test_score = [-0.00, 0.73, 0.73, 0.73]
+    
     actual_train_score = regressor_df.loc["train_score"].tolist()
-    expected_train_score = [0.0, 0.7347815894858526, 0.7349114221789114, 0.734915756374242]
+    actual_train_score = [round(num, 2) for num in actual_train_score]
+    expected_train_score = [0.0, 0.73, 0.73, 0.73]
     assert actual_test_score == expected_test_score, "regressor modeled incorrectly test scores are not equal to what they should be!"
     assert actual_train_score == expected_train_score, "regressor modeled incorrectly train scores are not equal to what they should be!"
 
